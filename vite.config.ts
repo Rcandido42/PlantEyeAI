9058f8fdb32cd1c374eda04907c950a97c27fc3b
joaopeccanha18 <joaopeccanha18@users.noreply.github.com>
2026-04-29 22:52:44 +0100

    chore: increase build chunk size warning limit in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: { port: 3000, host: '0.0.0.0' },
+      build: { chunkSizeWarningLimit: 1600 },
       plugins: [
         react(), basicSsl(),
         VitePWA({
